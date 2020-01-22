f664fa83c1013644ccde64ef6b3abee7f88d2821
Francisco Carreiro <Francisco.Carreiro96@gmail.com>
2020-01-22 07:25:23 -0300

    Atualização dos scripts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const urlRouter = require("./routes/url.route");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(urlRouter)
@@ -13,4 +13,4 @@ app.use(urlRouter)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}!`));
